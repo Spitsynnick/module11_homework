a44655da90d37cdf2562b9f3bbbc981590bf6984
Spitsynnick <109472664+Spitsynnick@users.noreply.github.com>
2022-12-03 16:19:50 +0300

    Added comments to files index.js, index.test.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# module11_homework-
-unit-test for function from Module 5
+Homework on module 11 "Testing"
+
+Task 2: Unit-test for function repeatWord from module 5: function gets 2 parameters: word and count (number of word repeats).
+For example, after call repeatWord('word', 3), function prints "word1,word2,word3," in line. 
--- src/_tests_/index.test.js
+++ src/_tests_/index.test.js
@@ -2,69 +2,72 @@ import { repeatWord } from "../index.js";
 
 describe("tests for repeatWord function", () => {
     
-    // тестируем передачу функции строки "apple" и целого числа 3
+    // тестирование передачи функции строки "apple" и целого числа 3
     it("should return word 'apple' 3 times", () => { 
         const result = repeatWord("apple", 3);
         expect(result).toBe("apple1,apple2,apple3,");
     }),
 
-    // тестируем передачу функции строки "apple" и дробного числа 2.5
+    // тестирование передачи функции строки "apple" и дробного числа 2.5
     it("should return word 'apple' 2 times", () => { 
         const result = repeatWord("apple", 2.5);
         expect(result).toBe("apple1,apple2,");
     }),
 
-    // тестируем передачу функции строки "apple" и числа 0
+    // тестирование передачи функции строки "apple" и числа 0
     it("should return empty string", () => { 
         const result = repeatWord("apple", 0);
         expect(result).toBe("");
     }), 
 
-    // тестируем передачу фунции невалидных данных: строки "apple" и отрицательного числа
+    // тестирование передачи фунции невалидных данных: строки "apple" и отрицательного числа
     it("should return undefined", () => {
         const result = repeatWord("apple", -5); 
         expect(result).toBe(undefined);
     }),
 
-    // тестируем передачу фунции невалидных данных: строки "apple" и  логического значения
+    // тестирование передачи фунции невалидных данных: строки "apple" и  логического значения
     it("should return undefined", () => {
         const result = repeatWord("apple", false); 
         expect(result).toBe(undefined);
     }),
 
-    // тестируем передачу фунции невалидных данных: логического значения и положительного числа
+    // тестирование передачи фунции невалидных данных: логического значения и положительного числа
     it("should return boolean true converted to string 2 times", () => { 
         const result = repeatWord(true, 2); 
         expect(result).toBe("true1,true2,");
     }),
 
-    // тестируем передачу фунции невалидных данных: 2-х положительных чисел
+    // тестирование передачи фунции невалидных данных: 2-х положительных чисел
     it("should return number 10 converted to string 3 times", () => { 
         const result = repeatWord(10, 3); 
         expect(result).toBe("101,102,103,");
     }),
 
-    // тестируем передачу фунции невалидных данных: массива данных и положительного числа
+    // тестирование передачи фунции невалидных данных: массива данных и положительного числа
     it("should return array converted to string", () => { 
         const result = repeatWord([1, 2, 3], 1); 
         expect(result).toBe("1,2,31,");
     }),
 
-    // тестируем передачу фунции невалидных данных: объекта и положительного числа
+    // тестирование передачи фунции невалидных данных: объекта и положительного числа
     it("should return object converted to string", () => {
         const result = repeatWord({a: 1, b: 2}, 1); 
         expect(result).toBe("[object Object]1,");
     }),
 
-    // тестируем передачу фунции невалидных данных: функции и положительного числа
+    // тестирование передачи фунции невалидных данных: функции и положительного числа
     it("should return function converted to string", () => {
         const result = repeatWord(function testFunction(){}, 1); 
         expect(result).toBe("function testFunction() {}1,");
     })
 })
 
-// Результат: 
-// Test Suites: 1 passed, 1 total
-// Tests:       10 passed, 10 total
+
+/*
+    Результат: 
+    Test Suites: 1 passed, 1 total
+    Tests:       10 passed, 10 total
+*/
 
 
